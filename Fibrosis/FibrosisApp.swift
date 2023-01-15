07741fca5792103f6630091387c9a1ab58efadae
Rafael Neuwirth Swierczynski <channelrns@gmail.com>
2023-01-15 19:03:29 -0300

    Initial Commit

---
+++ Fibrosis/FibrosisApp.swift
@@ -0,0 +1,17 @@
+//
+//  FibrosisApp.swift
+//  Fibrosis
+//
+//  Created by Rafael Neuwirth Swierczynski on 15/01/23.
+//
+
+import SwiftUI
+
+@main
+struct FibrosisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
